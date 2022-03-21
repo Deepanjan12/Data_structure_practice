3b986f0be75f925e631783f8cf7f6db2711feb6b
Deepanjan Sur <deepanjansur12@gmail.com>
2022-03-22 00:57:18 +0530

    New Program Added

--- array/job_sequence.c
+++ array/job_sequence.c
@@ -1,93 +1,139 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct work
+
+#define MAX 100
+
+typedef struct Job
 {
-    char job[20];
+    char id[5];
     int deadline;
     int profit;
-};
-int sort(struct work a[], int n)
+} Job;
+
+void jobSequencingWithDeadline(Job jobs[], int n);
+
+int minValue(int x, int y)
 {
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (a[j].profit < a[j + 1].profit)
-            {
-                struct work temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
-    return 0;
+    if (x < y)
+        return x;
+    return y;
 }
-int main()
+
+int main(void)
 {
-    int n;
-    printf("\nEnter the number of jobs: ");
+
+    int i, j, n;
+    printf("Enter the number of jobs: ");
     scanf("%d", &n);
-    struct work a[n];
-    for (int i = 0; i < n; i++)
+    Job jobs[n];
+    for (i = 0; i < n; i++)
     {
         printf("\nEnter the details of job %d: ", i + 1);
         printf("\nEnter the name of the job: ");
-        scanf("%s", a[i].job);
+        scanf("%s", jobs[i].id);
         printf("\nEnter the deadline of the job: ");
-        scanf("%d", &a[i].deadline);
+        scanf("%d", &jobs[i].deadline);
         printf("\nEnter the profit of the job: ");
-        scanf("%d", &a[i].profit);
+        scanf("%d", &jobs[i].profit);
     }
-    sort(a, n);
-    printf("\nThe jobs in order of profit are: ");
-    int temp[n];
-    for (int i = 0; i < n; i++)
+
+    Job temp;
+
+    for (i = 1; i < n; i++)
     {
-        temp[i] = a[i].profit;
-        printf("\n%s", a[i].job);
-    }
-    printf("\n");
-    int out[n + 1];
-    for (int i = 0; i <= n; i++)
-    {
-        out[i] = -1;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        int pos = a[i].deadline;
-        if (out[pos] == -1)
+        for (j = 0; j < n - i; j++)
         {
-            out[pos] = i;
-        }
-        else
-        {
-            for (int j = 1; j < pos; j++)
+            if (jobs[j + 1].profit > jobs[j].profit)
             {
-                if (out[j] == -1)
-                {
-                    out[j] = i;
-                    break;
-                }
+                temp = jobs[j + 1];
+                jobs[j + 1] = jobs[j];
+                jobs[j] = temp;
             }
         }
     }
-    int sum = 0;
-    printf("\nOptimal Schedule is: ");
-    for (int i = 0; i <= n; i++)
+
+    printf("%10s %d %d\n", "Job", "Deadline", "Profit");
+    for (i = 0; i < n; i++)
     {
-        if (out[i] != -1)
-        {
-            printf("\n%s", a[out[i]].job);
-            sum += a[out[i]].profit;
-        }
+        printf("%10s %d %d\n", jobs[i].id, jobs[i].deadline, jobs[i].profit);
     }
-    printf("\nmaximum earned profit: %d", sum);
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i].deadline == out[i])
-        {
-            printf("\nThe job %s canont be completed within deadline", a[i].job);
-        }
-    }
+
+    jobSequencingWithDeadline(jobs, n);
+
     return 0;
+}
+
+void jobSequencingWithDeadline(Job jobs[], int n)
+{
+
+    int i, j = 0, k, maxprofit;
+
+    int timeslot[MAX];
+
+    int filledTimeSlot = 0;
+
+    int notprocessed[MAX / 2];
+
+    int dmax = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (jobs[i].deadline > dmax)
+        {
+            dmax = jobs[i].deadline;
+        }
+    }
+
+    for (i = 1; i <= dmax; i++)
+    {
+        timeslot[i] = -1;
+    }
+
+    printf("dmax: %d\n", dmax);
+
+    for (i = 1; i <= n; i++)
+    {
+        k = minValue(dmax, jobs[i - 1].deadline);
+        while (k >= 1)
+        {
+            if (timeslot[k] == -1)
+            {
+                timeslot[k] = i - 1;
+                filledTimeSlot++;
+                break;
+            }
+            k--;
+        }
+        if (k < 1)
+        {
+            notprocessed[j] = i;
+        }
+
+        if (filledTimeSlot == dmax)
+        {
+            if (i < n)
+            {
+                notprocessed[j] = i;
+            }
+            break;
+        }
+    }
+
+    printf("\nRequired Jobs: ");
+    for (i = 1; i <= dmax; i++)
+    {
+        printf("%s", jobs[timeslot[i]].id);
+
+        if (i < dmax)
+        {
+            printf(" --> ");
+        }
+    }
+
+    maxprofit = 0;
+    for (i = 1; i <= dmax; i++)
+    {
+        maxprofit += jobs[timeslot[i]].profit;
+    }
+    printf("\nMax Profit: %d\n", maxprofit);
+
+    printf("\nJobs not processed: %s", jobs[notprocessed[j]].id);
 }
